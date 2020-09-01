c2564fe081b29c24ca18c2aa11e2dcb98dd5d1a5
eureyuri <eureynoguchi@gmail.com>
2020-09-01 12:01:43 -0400

    Finished half cover styling

--- src/components/halfCover.js
+++ src/components/halfCover.js
@@ -11,12 +11,22 @@ export default function HalfCover({ coverImage }) {
                 style={{
                     position: "absolute",
                     width: "100vw",
+                    minWidth: "100vw",
                     maxHeight: "70vh",
-                    right: "0"
+                    right: "0",
+                    zIndex: "-1",
                 }}
                 objectFit="cover"
             />
-            More About Eurey
+            <div className="halfcover__text">
+                <h1 className="halfcover__text__title">More About Eurey</h1>
+                <p>Space for showcasing my creative side</p>
+                <div className="halfcover__text__links">
+                    <h3 className="halfcover__text__link">Background</h3>
+                    <h3 className="halfcover__text__link">Interests</h3>
+                    <h3 className="halfcover__text__link">Blog</h3>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/more.js
+++ src/pages/more.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout";
-import HalfCover from "../components/halfCover"
+import HalfCover from "../components/halfCover";
+import Container from "../components/container";
 
 export default function More({ data }) {
 
@@ -8,6 +9,29 @@ export default function More({ data }) {
     <div>
       <Layout>
         <HalfCover coverImage={data.coverImage.childImageSharp.fluid} />
+
+        <div id="about" style={{background: "white", display: "flex", flexDirection:"column", justifyContent: "center", zIndex: "2", position: "relative", marginTop: "-0.5rem"}}>
+          <Container header="Background">
+            <div className="index__container">
+              <div className="index__left">
+              </div>
+              <div className="index__right">
+                <p className="index__text">
+                  I am a senior at Columbia University majoring in Computer Engineering seeking opportunities to work in the U.S.
+                </p>
+                <p className="index__text">
+                  I am a full stack developer with a passion for <span className="highlight">web development</span> and <span className="highlight">design</span>. Recently, I have been working on the front-end with React.
+                </p>
+                <p className="index__text">
+                  My life is all about app development for social impact, design, video, and music!
+                </p>
+
+              </div>
+            </div>
+          </Container>
+        </div>
+
+
       </Layout>
     </div>
 
@@ -16,7 +40,7 @@ export default function More({ data }) {
 
 export const query = graphql`
   query {
-    coverImage: file(relativePath: { eq: "cover2.jpeg" }) {
+    coverImage: file(relativePath: { eq: "cover4.jpeg" }) {
       childImageSharp {
         fluid(maxWidth: 1000) {
           ...GatsbyImageSharpFluid
