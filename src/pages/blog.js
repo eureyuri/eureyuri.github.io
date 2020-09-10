4cb0ff8cdaeb88c03c19de25a3f6f629202063a0
eureyuri <eureynoguchi@gmail.com>
2020-09-09 22:09:03 -0400

    Using cards to display blog post. Refactoring half cover component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,12 +3,17 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Container from '../components/container'
 import HalfCover from '../components/halfCover'
+import ProjectCard from '../components/ProjectCard'
 
 const BlogPost = ({node}) => {
     return (
-        <li>
-            <Link to={node.slug}>{node.title}</Link>
-        </li>
+        <Link to={node.slug}>
+            <ProjectCard
+                image={node.heroImage.file.url}
+                heading={node.title}
+                secondaryHeading={`${node.publishDate}`.split("-").join(" ")}
+                body={`${node.description.description}`} />
+        </Link>
     )
 }
 
@@ -19,10 +24,10 @@ export default function blog({data}) {
                 <HalfCover title={"Blog"} />
                 <div style={{background: "white", display: "flex", flexDirection:"column", justifyContent: "center", zIndex: "2", position: "relative", marginTop: "-0.5rem"}}>
                     <Container>
-                        <ul>
+                        <div style={{display:"flex", flexWrap:"wrap", justifyContent:"center"}}>
                             {data.allContentfulBlog.edges.map(edge =>
                                 <BlogPost node={edge.node} />)}
-                        </ul>
+                        </div>
                     </Container>
                     </div>
             </Layout>
@@ -32,13 +37,27 @@ export default function blog({data}) {
 
 export const pageQuery = graphql`
     query pageQuery {
-        allContentfulBlog(filter: {
+        allContentfulBlog(
+            filter: {
             node_locale: {eq: "en-US"}
-        }) {
+            },
+            sort: {
+                fields: [publishDate], order: DESC
+            }
+        ) {
             edges {
                 node {
                     title
                     slug
+                    description {
+                        description
+                    }
+                    heroImage {
+                        file {
+                            url
+                        }
+                    }
+                    publishDate
                 }
             }
         }
