5c305944ef873360d90e5b3c7d03ab407033b5f3
eureyuri <eureynoguchi@gmail.com>
2020-09-13 15:37:29 -0400

    SEO settings

--- src/utils/Head.js
+++ src/utils/Head.js
@@ -10,16 +10,16 @@ export default () => (
 
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://eureyuri.codes/" />
+        <meta property="og:url" content="https://eureyuri.codes/" />
         <meta property="og:title" content="Eurey Noguchi" />
         <meta property="og:description" content="Personal website, portfolio, and blog for Eurey (Yuri) Noguchi." />
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="og:image" content="../images/favicon.png" />
 
         {/* <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="http://eureyuri.codes/" />
+        <meta property="twitter:url" content="https://eureyuri.codes/" />
         <meta property="twitter:title" content="Eurey Noguchi" />
         <meta property="twitter:description" content="Personal website, portfolio, and blog for Eurey (Yuri) Noguchi." />
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="twitter:image" content="../images/favicon.png" />
     </Helmet>
 )
